31f597c1c51c12b153f2615fb884272b19774394
Gurkamal Singh <info@gurkamal.io>
2020-01-12 01:27:14 +0000

    pass namespace on cmd line

--- kubernetes/storage/efs/install-efs-provisioner.sh
+++ kubernetes/storage/efs/install-efs-provisioner.sh
@@ -2,8 +2,11 @@
 
 source ./.efs-provisioner.config
 
+kubectl create namespace efs-provisioner
+
 helm install efs-provisioner ./efs-provisioner \
     --set global.deployEnv=$DEPLOY_ENV \
     --set efsProvisioner.efsFileSystemId=$EFS_FILE_SYSTEM_ID \
     --set efsProvisioner.awsRegion=$AWS_REGION \
-    --set efsProvisioner.provisionerName=$PROVISIONER_NAME
+    --set efsProvisioner.provisionerName=$PROVISIONER_NAME \
+    --namespace $NAMESPACE
