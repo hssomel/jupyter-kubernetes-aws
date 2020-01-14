f1fd245639436ce836a3827dd34f1cfabde29b18
Gurkamal Singh <info@gurkamal.io>
2020-01-14 20:22:09 +0000

    change directory path

--- kill-everything.sh
+++ kill-everything.sh
@@ -1,7 +1,7 @@
 
-kubectl delete pvc --all --all-namespaces
-kubectl delete ns efs-provisioner
-source ~/jupyter-kubernetes-aws/kubernetes/persistent-volume/efs/delete-efs-aws.sh
+#kubectl delete pvc --all --all-namespaces
+#kubectl delete ns efs-provisioner
+source ~/jupyter-kubernetes-aws/kubernetes/persistent-volume/aws-efs/delete-efs-aws.sh
 source ~/jupyter-kubernetes-aws/aws/kill-kubernetes.sh
 
 
