3b58e16abd9f9012acd9a4115b128b4bf589bc2e
Gurkamal Singh <info@gurkamal.io>
2020-01-14 20:12:38 +0000

    configure ebs gp2 storage class to be non-default

---
+++ kubernetes/persistent-volume/aws-ebs/adjust-ebs-storage-class.sh
@@ -0,0 +1,9 @@
+echo "
+################################################################################
+# MAKE AWS EBS STORAGECLASS NON-DEFAULT
+################################################################################
+"
+kubectl delete storageclass default
+kubectl delete storageclass gp2
+kubectl apply -f \
+  ~/jupyter-kubernetes-aws/kubernetes/persistent-volume/aws-ebs/ebs-gp2-storage-class.yaml
