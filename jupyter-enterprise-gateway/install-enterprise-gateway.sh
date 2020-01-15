8a93d59b6e75e49eb4a360c6d720787c557926c4
Gurkamal Singh <info@gurkamal.io>
2020-01-15 06:53:10 +0000

    made script resuable for upgrades during develpoment

--- jupyter-enterprise-gateway/install-enterprise-gateway.sh
+++ jupyter-enterprise-gateway/install-enterprise-gateway.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-kubectl create namespace enterprise-gateway
+echo "
+################################################################################
+# INSTALL OR UPGRADE ENTERPRISE GATEWAY WITH HELM
+################################################################################
+"
 
-helm install \
+NAMESPACE=$( \
+  kubectl get namespaces | grep "enterprise-gateway" \
+)
+
+if [[ -z $NAMESPACE ]]
+then
+  kubectl create namespace enterprise-gateway
+fi
+
+RELEASE=enterprise-gateway-dev
+
+helm upgrade $RELEASE \
   ~/jupyter-kubernetes-aws/jupyter-enterprise-gateway/enterprise-gateway \
-  --generate-name \
+  --install \
+  --atomic \
   --namespace enterprise-gateway \
   --devel \
   --values ~/jupyter-kubernetes-aws/jupyter-enterprise-gateway/.values.yaml
