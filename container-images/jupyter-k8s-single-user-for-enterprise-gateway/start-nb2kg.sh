0ba39db8fbb53afec739d2c34e02a9ee90e091ce
Gurkamal Singh <info@gurkamal.io>
2020-01-15 22:09:28 +0000

    need custom container on top of k8s-single-user-sample to run the connection properly

---
+++ container-images/jupyter-k8s-single-user-for-enterprise-gateway/start-nb2kg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+https://github.com/jupyter/enterprise_gateway/blob/master/etc/docker/nb2kg/start-nb2kg.sh
+
+export NB_PORT=${NB_PORT:-8888}
+export GATEWAY_HOST=${GATEWAY_HOST:-localhost}
+export KG_URL=${KG_URL:-http://${GATEWAY_HOST}:${NB_PORT}}
+export KG_HTTP_USER=${KG_HTTP_USER:-jovyan}
+export KG_REQUEST_TIMEOUT=${KG_REQUEST_TIMEOUT:-30}
+export KERNEL_USERNAME=${KG_HTTP_USER}
+
+
+echo "Starting nb2kg against gateway: " ${KG_URL}
+echo "Nootbook port: " ${NB_PORT}
+echo "Kernel user: " ${KERNEL_USERNAME}
+
+echo "${@: -1}"
+
+
+exec /usr/local/bin/start-notebook.sh $*
