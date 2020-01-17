f473cdfc44c2a57c22927d66f90d92060fb6e33c
hssomel <somel.harpreet@gmail.com>
2020-01-17 00:03:54 +0000

    change extension for vim

---
+++ jupyterhub-kubernetes/eg-and-jh-install-jupyterhub.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+NAMESPACE=$1
+
+echo "
+################################################################################
+# INSTALL OR UPGRADE JUPYTERHUB  WITH HELM
+################################################################################
+"
+
+CHECK_NAMESPACE=$( \
+  kubectl get namespaces | grep "^${NAMESPACE} " \
+)
+
+if [[ -z $CHECK_NAMESPACE ]]
+then
+  kubectl create namespace $NAMESPACE
+fi
+
+RELEASE=jupyterhub-dev-$NAMESPACE
+
+helm upgrade $RELEASE \
+  ~/jupyter-kubernetes-aws/jupyterhub-kubernetes/jupyterhub \
+  --namespace $NAMESPACE \
+  --devel \
+  --atomic \
+  --values ~/jupyter-kubernetes-aws/jupyterhub-kubernetes/.eg-and-jh-values.yaml \
+  --install
